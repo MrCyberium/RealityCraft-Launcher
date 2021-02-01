129127a435aae092a7f6af7e5a5914ad1d877d4d
BuildTools <unconfigured@null.spigotmc.org>
2021-02-01 13:03:08 +0100

    Fixs mineurs

--- app/assets/js/scripts/uicore.js
+++ app/assets/js/scripts/uicore.js
@@ -108,7 +108,7 @@ function showUpdateUI(info){
     //TODO Make this message a bit more informative `${info.version}`
     document.getElementById('image_seal_container').setAttribute('update', true)
     document.getElementById('image_seal_container').onclick = () => {
-        setOverlayContent('Update Available', 'A new update for the launcher is available. Would you like to install now?', 'Install', 'Later')
+        setOverlayContent('Mise à jour disponible', 'Une nouvelle mise à jour pour le launcher est disponible. Souhaitez-vous l\'installer maintenant ?', 'Installer', 'Plus tard')
         setOverlayHandler(() => {
             if(!isDev){
                 ipcRenderer.send('autoUpdateAction', 'installUpdateNow')
